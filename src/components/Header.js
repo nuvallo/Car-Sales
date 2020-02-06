f183bb403bf357d5d3ade0ec40b5c77e3dce1afc
Tyler <nuvallo.inc@gmail.com>
2020-02-06 16:12:33 -0500

    addedFeatues, header, and app updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Header = props => {
+  const name = useSelector(state => state.car.name);
+  const image = useSelector(state => state.car.image);
+  const price = useSelector(state => state.car.price);
+  console.log(props);
   return (
     <>
-      <figure className="image is-128x128">
-        <img src={props.car.image} alt={props.car.name} />
+      <figure className="image">
+        <img src={image} alt={name} />
       </figure>
-      <h2>{props.car.name}</h2>
-      <p>Amount: ${props.car.price}</p>
+      <h2>{name}</h2>
+      <p>Amount: ${price}</p>
     </>
   );
 };
